44fc33886e9e578b97b5f5d0daa334f140dc0116
Daniil Miskevich <miskevich.daniil@gmail.com>
2025-10-30 14:01:21 +0300

    Add VRAM implementation, and basic memory errors (stuck at 0, stuck at 1).

--- back/include/vram.hpp
+++ back/include/vram.hpp
@@ -1,25 +1,53 @@
 #ifndef VRAM_HPP
 #define VRAM_HPP
 
+#include <array>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
+#include <unordered_map>
 
-template <typename Word>
 class Vram {
+   public:
+    enum ErrType {
+        NO = 0,
+        STUCK_AT_0,
+        STUCK_AT_1,
+        // TODO! expand on this
+    };
+
+    typedef uintptr_t Word;
+    typedef std::array<ErrType, sizeof(Word) * 8> WordErrs;
+
    public:
     Vram(size_t const word_count)
     : word_count(word_count), _data(new Word[word_count]) {};
+    // TODO implement copy constructor
 
-    Word read(size_t const addr);
-    void write(size_t const addr, Word const word);
+    /// Gets a word at `i`ndex of the ram with set errors applied.
+    Word read(size_t const i) const;
+    /// Writes a word at `i`ndex to the ram with set errors applied.
+    void write(
+        size_t const i,
+        Word const word
+    );  // don't try converting into an operator
 
-    inline Word const &operator[](size_t const addr) { return read(addr); }
+    inline ErrType get_error(size_t const i, unsigned const bit_i) {
+        return _errors[i][bit_i];
+    }
+    inline void
+    set_error(size_t const i, unsigned const bit_i, ErrType const err) {
+        _errors[i][bit_i] = err;
+    }
+
+    inline Word operator[](size_t const i) const { return read(i); }
 
    public:
     size_t const word_count;
 
    private:
     Word *const _data;
+    std::unordered_map<size_t, WordErrs> _errors = {};
 };
 
 #endif
--- back/src/main.cpp
+++ back/src/main.cpp
@@ -1,6 +1,37 @@
+
 #include <cstdio>
 
+#include "conviniences.hpp"
+#include "vram.hpp"
+
+#define PRINT_COLS (4)
+
+static int printword(Vram::Word const word) {
+    return printf(
+        "%c%c%c%c ",
+        (uint8_t)(word >> 24),
+        (uint8_t)(word >> 16),
+        (uint8_t)(word >> 8),
+        (uint8_t)(word >> 0)
+    );
+}
+
 int main() {
-    printf("Hello world!");
+    Vram vram(32);
+
+    char const str[] = "Hello world!";
+
+    for (unsigned i = 0; i < lenof(str); i++) vram.write(i, str[i]);
+    vram.set_error(3, 0, Vram::STUCK_AT_1);
+    vram.set_error(4, 3, Vram::STUCK_AT_0);
+
+    for (unsigned i = 0; i < vram.word_count / PRINT_COLS; i++) {
+        for (unsigned j = 0; j < PRINT_COLS; j++)
+            printf("%08lX ", vram.read(i * PRINT_COLS + j));
+        printf("| ");
+        for (unsigned j = 0; j < PRINT_COLS; j++)
+            printword(vram.read(i * PRINT_COLS + j));
+        printf("\n");
+    }
     return 0;
 }
