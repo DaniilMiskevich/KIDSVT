5e14e07c6cf531d42133003c5d6e7cd88dd27e88
Daniil Miskevich <miskevich.daniil@gmail.com>
2025-11-11 18:24:42 +0300

    Baisc vmachine function implemented. Still a bit far from ready.

--- back/include/vram.hpp
+++ back/include/vram.hpp
@@ -20,30 +20,24 @@ class Vram {
     typedef std::array<ErrType, sizeof(Word) * 8> WordErrs;
 
    public:
-    Vram(size_t const word_count)
-    : word_count(word_count), _data(new Word[word_count]) {};
+    Vram(size_t const word_count) : len(word_count), _data(new Word[word_count]) {};
     // TODO implement copy constructor
 
     /// Gets a word at `i`ndex of the ram with set errors applied.
     Word read(size_t const i) const;
     /// Writes a word at `i`ndex to the ram with set errors applied.
-    void write(
-        size_t const i,
-        Word const word
-    );  // don't try converting into an operator
+    void write(size_t const i,
+               Word const word);  // don't try converting into an operator
 
-    inline ErrType get_error(size_t const i, unsigned const bit_i) {
-        return _errors[i][bit_i];
-    }
-    inline void
-    set_error(size_t const i, unsigned const bit_i, ErrType const err) {
+    inline ErrType get_error(size_t const i, unsigned const bit_i) { return _errors[i][bit_i]; }
+    inline void set_error(size_t const i, unsigned const bit_i, ErrType const err) {
         _errors[i][bit_i] = err;
     }
 
     inline Word operator[](size_t const i) const { return read(i); }
 
    public:
-    size_t const word_count;
+    size_t const len;
 
    private:
     Word *const _data;
--- back/src/main.cpp
+++ back/src/main.cpp
@@ -1,7 +1,9 @@
 
 #include <cstdio>
+#include <string>
 
 #include "conviniences.hpp"
+#include "vmach.hpp"
 #include "vram.hpp"
 
 #define PRINT_COLS (4)
@@ -16,6 +18,18 @@ static int printword(Vram::Word const word) {
     );
 }
 
+char const *const demoprogram = R"END(
+        0 1
+        2 LOOP DESC
+            SWAP
+            0 LOOP 
+                CUR WRITE
+                CUR READ EQUAL?
+                ASSERT!
+            ASC ENDLOOP
+        ENDLOOP
+    )END";
+
 int main() {
     Vram vram(32);
 
@@ -25,12 +39,13 @@ int main() {
     vram.set_error(3, 0, Vram::STUCK_AT_1);
     vram.set_error(4, 3, Vram::STUCK_AT_0);
 
-    for (unsigned i = 0; i < vram.word_count / PRINT_COLS; i++) {
-        for (unsigned j = 0; j < PRINT_COLS; j++)
-            printf("%08lX ", vram.read(i * PRINT_COLS + j));
+    Vmach vmach("2 LOOP DESC    1 ASSERT!   ENDLOOP", vram);
+    for (size_t i = 0; i < 20; i++) vmach.step();
+
+    for (unsigned i = 0; i < vram.len / PRINT_COLS; i++) {
+        for (unsigned j = 0; j < PRINT_COLS; j++) printf("%08lX ", vram.read(i * PRINT_COLS + j));
         printf("| ");
-        for (unsigned j = 0; j < PRINT_COLS; j++)
-            printword(vram.read(i * PRINT_COLS + j));
+        for (unsigned j = 0; j < PRINT_COLS; j++) printword(vram.read(i * PRINT_COLS + j));
         printf("\n");
     }
     return 0;
